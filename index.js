141d9e9b4bee35492936f32469e73b4f98a7f702
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-03 16:42:10 +0800

    got the math to work

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // real life counter part price (40000) divided by the Vector engine price (7)
-const usd = 40000 / 7
+const usd = 40000000 / 18000
 
 function koinToUSD(x){
     return Math.round((usd * x) * 100) / 100
 }
 
 function usdToKoin(x){
-    return Math.round((7 / x * 40000) * 100) / 100
+    return Math.round((x / usd) * 100) / 100
 }
 
 $(document).ready(function(){
