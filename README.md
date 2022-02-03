f14817544fac4a7b2e5eb08349f55deeb75a359f
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-03 16:16:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# kOIN
+Convert KSP funds to USD
+
+## License
+MIT
+
+## Features
+- Dark mode/Light mode switching
+- Convert KSP funds to USD and vice versa
---
+++ index.js
@@ -0,0 +1,30 @@
+// real life counter part price (40000) divided by the Vector engine price (7)
+const usd = 40000 / 7
+
+function koinToUSD(x){
+    return Math.round((usd * x) * 100) / 100
+}
+
+function usdToKoin(x){
+    return Math.round(((40000 * 7) / x) * 100) / 100
+}
+
+$(document).ready(function(){
+    console.log("Ready!")
+
+    $("#container").on()
+
+    $("#koin").on("input", function(){
+        $("#usd").val(
+            koinToUSD($("#koin").val())
+        )
+        console.log("Input")
+    })
+    
+    $("#usd").on("input", function(){
+        $("#koin").val(
+            usdToKoin($("#usd").val())
+        )
+        console.log("Input")
+    })
+})
